e5d98a58a9582dad526dc32d17be61e589787111
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2025-03-12 10:22:04 -0300

    add Comment component with styling; integrate into Post component and update global styles

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,3 +1,4 @@
+import { Comment } from '../Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -16,9 +17,9 @@ export function Post() {
 			</header>
 
 			<div className={styles.content}>
-				<p>Fala galeraa 👋</p>
-				<p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-				<p><a href="">jane.design/doctorcare</a></p>
+				<p>Lorem, ipsum dolor. 👋</p>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis voluptas qui natus autem laudantium alias molestias pariatur libero asperiores. Deserunt ea odio nemo quo. Aperiam placeat id dolorem magni. Voluptatibus! 🚀</p>
+				<p><a href="">lorem.ipsum/dolor</a></p>
 				<p>
 					<a href="">#novoprojeto</a>{' '}
 					<a href="">#nlw</a>{' '}
@@ -37,6 +38,13 @@ export function Post() {
 					<button type="submit">Publicar</button>
 				</footer>
 			</form>
+
+			<div className={styles.commentList}>
+				<Comment />
+				<Comment />
+				<Comment />
+			</div>
+
 		</article>
 	)
 }
