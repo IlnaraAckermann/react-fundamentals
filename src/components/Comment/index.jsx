3ad559febd9283a549ab55c0ea64bd5d366ad3f1
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2025-03-14 15:29:40 -0300

    add Avatar component with styling; update Post and Sidebar components to use Avatar

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar';
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
@@ -5,7 +6,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://avatars.githubusercontent.com/u/86618553?v=4" />
+      <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/86618553?v=4" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css';
+import { Avatar } from '../Avatar';
 
 
 export function Sidebar() {
@@ -12,7 +13,7 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
-				<img className={styles.avatar} src="https://avatars.githubusercontent.com/u/86618553?v=4" />
+				<Avatar src="https://avatars.githubusercontent.com/u/86618553?v=4" />
 
 				<strong>Ilnara Ackermann</strong>
 				<span>Web Developer</span>
